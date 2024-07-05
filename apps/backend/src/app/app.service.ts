7a2778773cfad1fc8b03ee7e66d61002aded956b
soldev <soldev@soldevs-MBP.attlocal.net>
2024-07-05 15:26:32 -0400

    add deploy

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,19 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { JsonRpcProvider, ethers } from 'ethers';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class AppService implements OnModuleInit {
   private provider: ethers.JsonRpcProvider;
   private contract: ethers.Contract;
 
-  private readonly contractAddress =
-    '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  private readonly contractAddress = process.env.HELLO_WORLD_CONTRACT_ADDRESS;
   private readonly contractABI = [
     'function helloWorld() public view returns (string memory)',
     'function setText(string memory newText) public',
   ];
 
   async onModuleInit() {
+    console.log('foo', this.contractAddress);
     this.provider = new JsonRpcProvider('http://localhost:8545', undefined, {
       staticNetwork: true,
     });
